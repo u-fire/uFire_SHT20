1469f6d7606fccbc60e02322149262a742feef17
Justin Decker <justin@ufire.co>
2020-05-08 07:16:13 -0700

    wire - _i2cPort fix

--- src/uFire_SHT20.cpp
+++ src/uFire_SHT20.cpp
@@ -11,23 +11,23 @@ bool uFire_SHT20::begin(uint8_t resolution, uint8_t address, TwoWire &wirePort)
 }
 void uFire_SHT20::_reset()
 {
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_RESET);
-  Wire.endTransmission();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_RESET);
+  _i2cPort->endTransmission();
   delay(SOFT_RESET_DELAY);
   _onchip_heater = _DISABLE_ONCHIP_HEATER;
   _otp_reload = _DISABLE_OTP_RELOAD;
 
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_READ_USER_REG);
-  Wire.endTransmission();
-  Wire.requestFrom(SHT20_I2C, 1);
-  uint8_t config = Wire.read();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_READ_USER_REG);
+  _i2cPort->endTransmission();
+  _i2cPort->requestFrom(SHT20_I2C, 1);
+  uint8_t config = _i2cPort->read();
   config = ((config & _RESERVED_BITMASK) | _resolution | _onchip_heater | _otp_reload);
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_WRITE_USER_REG);
-  Wire.write(config);
-  Wire.endTransmission();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_WRITE_USER_REG);
+  _i2cPort->write(config);
+  _i2cPort->endTransmission();
 }
 
 void uFire_SHT20::measure_all()
@@ -40,13 +40,13 @@ void uFire_SHT20::measure_all()
 float uFire_SHT20::temperature()
 {
   _reset();
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_TEMP);
-  Wire.endTransmission();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_TEMP);
+  _i2cPort->endTransmission();
   delay(TEMPERATURE_DELAY);
-  Wire.requestFrom(SHT20_I2C, 2);
-  uint8_t msb = Wire.read();
-  uint8_t lsb = Wire.read();
+  _i2cPort->requestFrom(SHT20_I2C, 2);
+  uint8_t msb = _i2cPort->read();
+  uint8_t lsb = _i2cPort->read();
   uint16_t value = msb << 8 | lsb;
   tempC = value * (175.72 / 65536.0)- 46.85;
   tempF = ((value * (175.72 / 65536.0)- 46.85)  * 1.8) + 32;
@@ -56,13 +56,13 @@ float uFire_SHT20::temperature()
 float uFire_SHT20::temperature_f()
 {
   _reset();
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_TEMP);
-  Wire.endTransmission();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_TEMP);
+  _i2cPort->endTransmission();
   delay(TEMPERATURE_DELAY);
-  Wire.requestFrom(SHT20_I2C, 2);
-  uint8_t msb = Wire.read();
-  uint8_t lsb = Wire.read();
+  _i2cPort->requestFrom(SHT20_I2C, 2);
+  uint8_t msb = _i2cPort->read();
+  uint8_t lsb = _i2cPort->read();
   uint16_t value = msb << 8 | lsb;
   tempC = value * (175.72 / 65536.0)- 46.85;
   tempF = ((value * (175.72 / 65536.0)- 46.85)  * 1.8) + 32;
@@ -72,13 +72,13 @@ float uFire_SHT20::temperature_f()
 float uFire_SHT20::humidity()
 {
   _reset();
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_HUMID);
-  Wire.endTransmission();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_HUMID);
+  _i2cPort->endTransmission();
   delay(HUMIDITY_DELAY);
-  Wire.requestFrom(SHT20_I2C, 2);
-  uint8_t msb = Wire.read();
-  uint8_t lsb = Wire.read();
+  _i2cPort->requestFrom(SHT20_I2C, 2);
+  uint8_t msb = _i2cPort->read();
+  uint8_t lsb = _i2cPort->read();
   uint16_t value = msb << 8 | lsb;
   RH = value * (125.0 / 65536.0) - 6.0;
   return RH;
@@ -113,11 +113,11 @@ float uFire_SHT20::dew_point()
 
 bool uFire_SHT20::connected()
 {
-  Wire.beginTransmission(SHT20_I2C);
-  Wire.write(SHT20_READ_USER_REG);
-  Wire.endTransmission();
-  Wire.requestFrom(SHT20_I2C, 1);
-  uint8_t config = Wire.read();
+  _i2cPort->beginTransmission(SHT20_I2C);
+  _i2cPort->write(SHT20_READ_USER_REG);
+  _i2cPort->endTransmission();
+  _i2cPort->requestFrom(SHT20_I2C, 1);
+  uint8_t config = _i2cPort->read();
   
   if (config != 0xFF) {
     return true;
