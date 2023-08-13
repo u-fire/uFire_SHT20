77cc3f55ae444de197a82485c418d23c677a75d4
Shawn Nectar <shawnmctc@gmail.com>
2023-08-13 13:11:48 -0300

    [Deprecated] [try! macro] to [? operator]

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,21 +5,19 @@ use std::io::prelude::*;
 use ufire_sht20::*;
 
 fn main() {
-	println!("Isolated EC Probe Interface shell");
-	println!("type `help` for a list of commands");
-	let mut shell = Shell::new(());
+    println!("Isolated EC Probe Interface shell");
+    println!("type `help` for a list of commands");
+    let mut shell = Shell::new(());
 
-	shell.new_command_noargs("t", "Measure temperature", move |io, _| {
-		let mut sht20 = SHT20::new("/dev/i2c-3").unwrap();
-		try!(writeln!(io, "{}", sht20.temperature().unwrap()));
-		Ok(())
-	});
+    shell.new_command_noargs("t", "Measure temperature", move |io, _| {
+        let mut sht20 = SHT20::new("/dev/i2c-3")?;
+        writeln!(io, "{}", sht20.temperature()?).map_err(Into::into)
+    });
 
-	shell.new_command_noargs("h", "Measure humidity", move |io, _| {
-		let mut sht20 = SHT20::new("/dev/i2c-3").unwrap();
-		try!(writeln!(io, "{}", sht20.humidity().unwrap()));
-		Ok(())
-	});
+    shell.new_command_noargs("h", "Measure humidity", move |io, _| {
+        let mut sht20 = SHT20::new("/dev/i2c-3")?;
+        writeln!(io, "{}", sht20.humidity()?).map_err(Into::into)
+    });
 
     shell.run_loop(&mut ShellIO::default());
 }
